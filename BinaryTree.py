610577b1af08e2b3c0a29607764890076a291ec8
Ye Shuzhan <48856819+YeShuzhan@users.noreply.github.com>
2022-05-27 17:16:46 +0800

    Add files via upload

--- BinaryTree.py
+++ BinaryTree.py
@@ -12,7 +12,7 @@ import collections
 from collections.abc import Iterable
 
 global res
-res = []  # type: List
+res = []  # type: List[Any]
 global count
 T = TypeVar('T')
 K = TypeVar('K', bound=Union[str, int, float])
@@ -20,7 +20,7 @@ D = TypeVar('D', bound=Union[None, str, int, float])
 T12 = Union[K, D]
 
 
-class BinaryNode('Generic[K, D]'):  # The node of binary balanced tree
+class BinaryNode(object):  # The node of binary balanced tree
     # The construction method, the new nodes are all leaves, and the height is
     # 1
 
